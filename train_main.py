09772eb3badecf2cfa976c8dfc67fda7477fbe02
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-07 20:45:57 +0100

    Update train_main.py

--- train_main.py
+++ train_main.py
@@ -15,39 +15,50 @@ on these settings (ie. the data_period variable), so the 0.1T runs can be
 reproduced by dividing this value by 10.
 """
 
+from absl import app
+from absl import flags
+
 from curl import training
 
+flags.DEFINE_enum('dataset', 'mnist', ['mnist', 'omniglot'], 'Dataset.')
 
-training.run_training(
-   dataset='mnist',
-   output_type='bernoulli',
-   n_y=30,
-   n_y_active=1,
-   training_data_type='sequential',
-   n_concurrent_classes=1,
-   lr_init=1e-3,
-   lr_factor=1.,
-   lr_schedule=[1],
-   blend_classes=False,
-   train_supervised=False,
-   n_steps=100000,
-   report_interval=10000,
-   knn_values=[10],
-   random_seed=1,
-   encoder_kwargs={
-       'encoder_type': 'multi',
-       'n_enc': [1200, 600, 300, 150],
-       'enc_strides': [1],
-   },
-   decoder_kwargs={
-       'decoder_type': 'single',
-       'n_dec': [500, 500],
-       'dec_up_strides': None,
-   },
-   n_z=32,
-   dynamic_expansion=True,
-   ll_thresh=-200.0,
-   classify_with_samples=False,
-   gen_replay_type='fixed',
-   use_supervised_replay=False,
-)
+FLAGS = flags.FLAGS
+
+
+def main(unused_argv):
+  training.run_training(
+      dataset='mnist',
+      output_type='bernoulli',
+      n_y=30,
+      n_y_active=1,
+      training_data_type='sequential',
+      n_concurrent_classes=1,
+      lr_init=1e-3,
+      lr_factor=1.,
+      lr_schedule=[1],
+      blend_classes=False,
+      train_supervised=False,
+      n_steps=100000,
+      report_interval=10000,
+      knn_values=[10],
+      random_seed=1,
+      encoder_kwargs={
+          'encoder_type': 'multi',
+          'n_enc': [1200, 600, 300, 150],
+          'enc_strides': [1],
+      },
+      decoder_kwargs={
+          'decoder_type': 'single',
+          'n_dec': [500, 500],
+          'dec_up_strides': None,
+      },
+      n_z=32,
+      dynamic_expansion=True,
+      ll_thresh=-200.0,
+      classify_with_samples=False,
+      gen_replay_type='fixed',
+      use_supervised_replay=False,
+      )
+
+if __name__ == '__main__':
+  app.run(main)
