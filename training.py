1b6a643be3ff111f382d3b5a02c35e008fbc5873
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-08 20:19:33 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,18 +1,3 @@
-################################################################################
-# Copyright 2019 DeepMind Technologies Limited
-#
-#     Licensed under the Apache License, Version 2.0 (the "License");
-#     you may not use this file except in compliance with the License.
-#     You may obtain a copy of the License at
-#
-#         https://www.apache.org/licenses/LICENSE-2.0
-#
-#     Unless required by applicable law or agreed to in writing, software
-#     distributed under the License is distributed on an "AS IS" BASIS,
-#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#     limitations under the License.
-################################################################################
 """Script to train CURL."""
 
 import collections
@@ -22,12 +7,12 @@ from absl import logging
 import numpy as np
 from sklearn import neighbors
 import sonnet as snt
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_probability as tfp
 
-from curl import model
-from curl import utils
+import model
+import utils
 
 tfc = tf.compat.v1
 
