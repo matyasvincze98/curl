bb851a05a18e8cc3e145b987fb1b545fc1a337d2
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-09 16:32:59 +0100

    Update training.py

--- training.py
+++ training.py
@@ -8,10 +8,8 @@ import numpy as np
 from sklearn import neighbors
 import sonnet as snt
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.WARN)
 import tensorflow_datasets as tfds
 import tensorflow_probability as tfp
 
