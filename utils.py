5c37d65b126d21150106b586be1daaa8a2c64b85
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-09 16:30:53 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,10 +5,6 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import logging
-
-import logging
-logging.getLogger("tensorflow").setLevel(logging.WARNING)
-
 import tensorflow as tf
 import tensorflow_probability as tfp
 
