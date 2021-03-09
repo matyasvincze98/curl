730a9051d2fc3d865cde87ebb80fc7a17be87a57
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-09 16:28:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,8 @@ from __future__ import print_function
 
 from absl import logging
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import logging
-tf.get_logger().setLevel(logging.ERROR)
+logging.getLogger("tensorflow").setLevel(logging.WARNING)
 
 import tensorflow as tf
 import tensorflow_probability as tfp
