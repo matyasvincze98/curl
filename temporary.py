9c620cb3015e95281b25457a212ad0eead339040
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-10 20:59:26 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -6,3 +6,10 @@ ds_train_, ds_info_ = tfds.load(
       with_info=True,
       as_dataset_kwargs={'shuffle_files': False},
       **{})
+
+
+'''
+import pickle
+import pandas as pd
+patches_128k = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
+'''
