dc8df182768bc23bb00567100152afa6a8d2e8ad
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-10 20:58:49 +0100

    Create temporary.py

---
+++ temporary.py
@@ -0,0 +1,8 @@
+import tensorflow_datasets as tfds
+
+ds_train_, ds_info_ = tfds.load(
+      name='mnist',
+      split=tfds.Split.TRAIN,
+      with_info=True,
+      as_dataset_kwargs={'shuffle_files': False},
+      **{})
