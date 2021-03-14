6ff2e00c7cab12ad51ec91e1f4fad1d4f17e89d0
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-14 17:46:41 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -3,6 +3,14 @@ import tensorflow_datasets as tfds
 import pickle
 import pandas as pd
 
+
+patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
+patches_128k_df = pd.DataFrame.from_dict(patches_128k_dict)
+print(patches_128k_df.head())
+
+# patches_128k_tf = tf.data.Dataset.from_tensor_slices(patches_128k_pd)
+
+'''
 ds_train, ds_info = tfds.load(
       name='mnist',
       split=tfds.Split.TRAIN,
@@ -14,10 +22,4 @@ ds_numpy = tfds.as_numpy(ds_train)
 for ex in ds_numpy:
       print(ex)
       break
-
-patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
-print(patches_128k_dict['train_images'][0], patches_128k_dict['train_labels'][0])
-
-'''
-patches_128k_tf = tf.data.Dataset.from_tensor_slices(patches_128k_pd)
 '''
