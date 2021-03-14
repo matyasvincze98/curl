f882b7f55c3454a0fbed820abf34b9757e202cef
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-14 18:24:55 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 patches_128k_train_dict = {key: patches_128k_dict[key] for key in ['train_images', 'train_labels']}
-print(patches_128k_train_dict)
 patches_128k_train_df = pd.DataFrame.from_dict(patches_128k_train_dict['train_images'])
-print(patches_128k_train_df.head())
+patches_128k_train_ds = tf.data.Dataset.from_tensor_slices(patches_128k_train_pd)
 
-# patches_128k_tf = tf.data.Dataset.from_tensor_slices(patches_128k_pd)
+ds_numpy = tfds.as_numpy(patches_128k_train_ds)
+for ex in ds_numpy:
+      print(ex)
+      break
 
 '''
 ds_train, ds_info = tfds.load(
