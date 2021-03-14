945f2b43e6ec3abccac0b397c3689bd1aaf1fe9c
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-14 16:55:03 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -5,7 +5,7 @@ import pandas as pd
 patches_128k = tf.data.Dataset.from_tensor_slices(
       pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 )
-print(patches_128k)
+print(type(patches_128k))
 
 '''
 ds_train_, ds_info_ = tfds.load(
