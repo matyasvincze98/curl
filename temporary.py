4a27540dda464ff7101ca7be7870466f1b3ee8dc
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-14 16:46:02 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -6,20 +6,19 @@ import numpy as np
 #import skimage.io as io
 import tensorflow as tf
 
+import pickle
+import pandas as pd
+
+patches_128k = tf.data.Dataset.from_tensor_slices(
+      pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
+)
+print(patches_128k)
+
+'''
 ds_train_, ds_info_ = tfds.load(
       name='mnist',
       split=tfds.Split.TRAIN,
       with_info=True,
       as_dataset_kwargs={'shuffle_files': False},
       **{})
-
-print(type(ds_train_), ds_train_)
-print()
-print(type(ds_info_), ds_info_)
-
-
-'''
-import pickle
-import pandas as pd
-patches_128k = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 '''
