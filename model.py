bf6dd7fe9f916c2824ea2e5ac9f844dfa5a65c89
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-03-29 18:07:17 +0200

    Update model.py

--- model.py
+++ model.py
@@ -162,7 +162,7 @@ def data_decoder_fn(z,
     output_dist = lambda x: tfp.distributions.Bernoulli(logits=x)
     n_out_factor = 1
     out_shape = list(output_shape)
-  if output_type == 'gaussian':
+  elif output_type == 'gaussian':
     output_dist = lambda x: utils.generate_gaussian(logits=x, sigma_nonlin='softplus', sigma_param='var')
     n_out_factor = 1
     out_shape = list(output_shape)
