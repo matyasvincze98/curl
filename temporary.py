ffbbf490dcc1c378b9db24ffb0b41768c6dda852
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-04-06 21:49:26 +0200

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -4,29 +4,11 @@ import tensorflow_datasets as tfds
 import pickle
 import pandas as pd
 
-bernoulli = tfp.distributions.Bernoulli(logits=[0.1, 0.5, 0.9])
-normal = tfp.distributions.Normal(loc=[0.1, 0.5, 0.9], scale=[0.1, 0.5, 0.9])
 
-print(bernoulli(1, 1))
-print(normal(1, 1))
-
-
-
-
-
-
-
-
-'''
 patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 patches_128k_train_dict = {key: patches_128k_dict[key] for key in ['train_images', 'train_labels']}
 patches_128k_train_dict['train_images'] = patches_128k_train_dict['train_images'].reshape(-1, 48, 48, 1)
 patches_128k_train_ds = tf.data.Dataset.from_tensor_slices(patches_128k_train_dict)
-
-ds_numpy = tfds.as_numpy(patches_128k_train_ds)
-for ex in ds_numpy:
-      print(ex['train_images'].shape)
-      break
       
 ds_train, ds_info = tfds.load(
       name='mnist',
@@ -35,21 +17,5 @@ ds_train, ds_info = tfds.load(
       as_dataset_kwargs={'shuffle_files': False},
       **{})
 
-ds_numpy = tfds.as_numpy(ds_train)
-for ex in ds_numpy:
-      print(ex['image'].shape)
-      break
-
-
-ds_train, ds_info = tfds.load(
-      name='mnist',
-      split=tfds.Split.TRAIN,
-      with_info=True,
-      as_dataset_kwargs={'shuffle_files': False},
-      **{})
-
-ds_numpy = tfds.as_numpy(ds_train)
-for ex in ds_numpy:
-      print(ex)
-      break
-'''
+print(patches_128k_train_ds)
+print(ds_train, ds_info)
