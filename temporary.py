d6bde65abbaa19586332deb2f3d469f6c4c99aee
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-04-09 17:36:04 +0200

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -8,7 +8,7 @@ import pandas as pd
 patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 patches_128k_train_dict = {key: patches_128k_dict[key] for key in ['train_images', 'train_labels']}
 patches_128k_train_dict['train_images'] = patches_128k_train_dict['train_images'][:10].reshape(-1, 48, 48, 1)
-patches_128k_train_dict['train_labels'] = patches_128k_train_dict['train_labels'][:10]
+patches_128k_train_dict['train_labels'] = int(patches_128k_train_dict['train_labels'][:10])
 patches_128k_test_dict = {key: patches_128k_dict[key] for key in ['test_images', 'test_labels']}
 patches_128k_test_dict['test_images'] = patches_128k_test_dict['test_images'][:10].reshape(-1, 48, 48, 1)
 patches_128k_test_dict['test_labels'] = patches_128k_test_dict['test_labels'][:10]
