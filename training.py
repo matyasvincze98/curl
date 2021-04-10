e38cbbf724bd3bf4bdd9cd4647235a5c8b3b3563
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-04-10 19:41:21 +0200

    Update training.py

--- training.py
+++ training.py
@@ -111,11 +111,11 @@ def get_data_sources(dataset, dataset_kwargs, batch_size, test_batch_size,
   # Load training data sources
   patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
   patches_128k_train_dict = {key: patches_128k_dict[key] for key in ['train_images', 'train_labels']}
-  patches_128k_train_dict['train_images'] = patches_128k_train_dict['train_images'][:10].reshape(-1, 48, 48, 1)
-  patches_128k_train_dict['train_labels'] = patches_128k_train_dict['train_labels'][:10]
+  patches_128k_train_dict['train_images'] = patches_128k_train_dict['train_images'][:10000].reshape(-1, 48, 48, 1)
+  patches_128k_train_dict['train_labels'] = patches_128k_train_dict['train_labels'][:10000]
   patches_128k_test_dict = {key: patches_128k_dict[key] for key in ['test_images', 'test_labels']}
-  patches_128k_test_dict['test_images'] = patches_128k_test_dict['test_images'][:10].reshape(-1, 48, 48, 1)
-  patches_128k_test_dict['test_labels'] = patches_128k_test_dict['test_labels'][:10]
+  patches_128k_test_dict['test_images'] = patches_128k_test_dict['test_images'][:1000].reshape(-1, 48, 48, 1)
+  patches_128k_test_dict['test_labels'] = patches_128k_test_dict['test_labels'][:1000]
   train_ds = tf.data.Dataset.from_tensor_slices(patches_128k_train_dict)
   test_ds = tf.data.Dataset.from_tensor_slices(patches_128k_test_dict)
     
