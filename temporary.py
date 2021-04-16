2b9985fee60f478c988f56cb7b26165ac870cbc0
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-04-16 12:41:52 +0200

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -4,7 +4,6 @@ import tensorflow_datasets as tfds
 import pickle
 import pandas as pd
 
-'''
 patches_128k_dict = pd.read_pickle('https://wigner.hu/~fcsikor/textures/labeled_texture_oatleathersoilcarpetbubbles_subsamp1_filtered_128000_48px.pkl')
 patches_128k_train_dict = {key: patches_128k_dict[key] for key in ['train_images', 'train_labels']}
 patches_128k_train_dict['train_images'] = patches_128k_train_dict['train_images'][:10].reshape(-1, 48, 48, 1)
@@ -14,8 +13,15 @@ patches_128k_test_dict['test_images'] = patches_128k_test_dict['test_images'][:1
 patches_128k_test_dict['test_labels'] = patches_128k_test_dict['test_labels'][:10]
 train_ds = tf.data.Dataset.from_tensor_slices(patches_128k_train_dict)
 test_ds = tf.data.Dataset.from_tensor_slices(patches_128k_test_dict)
-'''
+
+ds_train, ds_info = tfds.load(
+      name=dataset,
+      split=tfds.Split.TRAIN,
+      with_info=True,
+      as_dataset_kwargs={'shuffle_files': False},
+      **dataset_kwargs)
       
+'''
 import training
 batch_size = 100
 test_batch_size = 1000
@@ -28,11 +34,10 @@ dataset_ops = training.get_data_sources('mnist', dataset_kwargs, batch_size,
 print(dataset_ops.train_data)
 print(dataset_ops.train_data_for_clf)
 print(dataset_ops.test_data)
-
 '''
+
 q = train_ds.make_one_shot_iterator().get_next()
 qq = ds_train.make_one_shot_iterator().get_next()
 with tf.Session() as sess:
       print(sess.run(q))
       print(sess.run(qq))
-'''
